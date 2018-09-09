a4670301e39b1badb0089a2a7b33164d6df1d9c0
[mmursalin] <[mursalin3152@gmail.com]>
2018-09-09 14:23:06 -0400

    finished fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -1,11 +1,20 @@
 package math.problems;
 
 public class Fibonacci {
+
+
     public static void main(String[] args) {
          /*
           Write 40 Fibonacci numbers with java.
          */
 
-
+        int i = 4;
+        int n = 1;
+    System.out.println("40 Fibonacci numbers are; ");
+    for (int t = 1; t<= 40; t++){
+        int sum = i + n;
+        n = sum;
+        System.out.print(sum + " ");
+    }
     }
 }
