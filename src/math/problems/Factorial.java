ea1e5a7e05c83cb42625ba35275a30658da39260
[mmursalin] <[mursalin3152@gmail.com]>
2018-09-09 14:11:04 -0400

    finished factorial

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,16 +1,33 @@
 package math.problems;
 
-/**
- * Created by mrahman on 04/02/18.
- */
-public class Factorial {
 
-    public static void main(String[] args) {
-        /*
-         * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
-         * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
-         *
-         */
+public class Factorial {
+    static int fact(int n)
+    {
+        int x;
+        if(n==1)
+        {
+            return 1;
+        }
+        else {
+            x = n*fact(n-1);
+            return x;
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        int number = 27;
+        int fact = 1;
+        int i;
+        for (i=1; i<= number;i++)
+        {
+            fact = fact * i;
+
+
+        }
+        System.out.println("The factorial of 27 is: " + fact);
 
     }
 }
