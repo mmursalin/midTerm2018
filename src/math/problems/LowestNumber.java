de7c9ea5721c67696c2aa648e5c3b558b105bfe2
[mmursalin] <[mursalin3152@gmail.com]>
2018-09-10 16:31:03 -0400

    wooooooooooooooo

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -7,28 +7,41 @@ import java.util.List;
 
 public class LowestNumber {
 
-	public static void main(String[] args) {
-		/*
-		 * Write java solution to find the lowest number from this array.
-		 * Use one of the databases from mysql or mongodb to store and to retrieve.
-		 */
-		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
+    public static void main(String[] args) {
+        /*
+         * Write java solution to find the lowest number from this array.
+         * Use one of the databases from mysql or mongodb to store and to retrieve.
+         */
+        int array[] = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
+        int smallest = Integer.MAX_VALUE;
 
-		//find lowest number from the array
+        for (int i = 0; i < array.length; i++) {
+            if (smallest > array[i]) {
+                smallest = array[i];
+            }
+        }
+        System.out.println("Smallest number in array is : " + smallest);
 
-		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-		List<String> lowestValue = new ArrayList<String>();
-		try {
-			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
-			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+        ConnectToSqlDB connectDB = new ConnectDB();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-		for(String st:lowestValue){
-			System.out.println(st);
-		}
-	}
+        List<String> lowestValue = new ArrayList<String>();
+        int[] array1 = new int[1];
+        array1[0] = smallest;
+        try {
+            ConnectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
+            lowestValue = connectDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+            System.out.println("Data is reading from the Table (tbl_lowestNumber) and displaying to the console");
+            for (String st : lowestValue) {
+                System.out.println(st);
+            }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class ConnectDB extends ConnectToSqlDB {
+    }
 }
+
+
